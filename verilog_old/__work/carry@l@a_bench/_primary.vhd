03448518c8b69b2f9f3f2af3d67dbd5b83cbca82
nambur <nambur@wisc.edu>
2013-04-11 15:54:00 -0500

    removed verilog_old and backUp dirs

--- verilog_old/__work/carry@l@a_bench/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity carryla_bench is
-end carryla_bench;
--- verilog_old/__work/execute_stage_bench/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity execute_stage_bench is
-end execute_stage_bench;
