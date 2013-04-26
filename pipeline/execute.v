0f633be2ef25375b17937acaee848b8af8d2d3c6
nambur <nambur@wisc.edu>
2013-04-26 16:30:06 -0500

    cleaned execute.v

--- pipeline/execute.v
+++ pipeline/execute.v
@@ -4,6 +4,7 @@ module execute(ALUSrc_IDEX,PC2_IDEX,ALUOp_IDEX,Rd1_IDEX,Rd2_IDEX,Imm_IDEX,ALUF_I
               ALUO_EXMEM,Rd2_EXMEM,MemtoReg_EXMEM,MemWrite_EXMEM,PC_IDEX,
               MemRead_EXMEM,Dump_EXMEM,halt_IDEX,halt_EXMEM,Jump_IDEX
               ,err,clk,rst);
+
     //Non-Pipelined signals 
     output err;
     input clk,rst;
@@ -62,10 +63,8 @@ module execute(ALUSrc_IDEX,PC2_IDEX,ALUOp_IDEX,Rd1_IDEX,Rd2_IDEX,Imm_IDEX,ALUF_I
     assign claIn = ((ALUOp_IDEX == 5'b00101) | (ALUOp_IDEX == 5'b00111)) ? Rd1_IDEX : PC_IDEX;
 
     assign slbiOut = (Rd1_IDEX << 4'h8) | Imm_IDEX;
-    //neg-to-pos and pos-to-neg detection
     assign sltOut = (Rd2_IDEX[15]&(~Rd1_IDEX[15])) ? 16'h0000 : (((~Rd2_IDEX[15])&Rd1_IDEX[15]) ? 
                     16'h0001 : (flag[2] ? 16'h0001 : 16'h0000));
-    //assign sltOut = (flag[2]) ? 16'h0001 : 16'h0000;
     assign btrOut = {Rd1_IDEX[0],Rd1_IDEX[1],Rd1_IDEX[2],Rd1_IDEX[3],Rd1_IDEX[4],Rd1_IDEX[5],Rd1_IDEX[6],Rd1_IDEX[7],Rd1_IDEX[8],Rd1_IDEX[9],
                     Rd1_IDEX[10],Rd1_IDEX[11],Rd1_IDEX[12],Rd1_IDEX[13],Rd1_IDEX[14],Rd1_IDEX[15]};
     assign scoOut = (CO) ? 16'h0001 : 16'h0000;
@@ -74,8 +73,7 @@ module execute(ALUSrc_IDEX,PC2_IDEX,ALUOp_IDEX,Rd1_IDEX,Rd2_IDEX,Imm_IDEX,ALUF_I
                     16'h0001 : (flag[2] ? 16'h0001 : 16'h0000)));
 
     //Added for STU -- change to route through adder
-    assign temp = Imm_IDEX + Rd1_IDEX;
-    assign stuOut = temp;
+    assign stuOut = Imm_IDEX + Rd1_IDEX;
 
      alu THEALU(.A(Rd1_IDEX), .B(bin), .Op(opOut), .invA(1'b0), .invB(invB), .sign(1'b1), 
         .Out(outALU), .Ofl(flag[1]), .Z(flag[0]), .CO(CO), .err(aluerr));
